dab7e2543fcd852ad9346e253124d6953d5409a0
Karolis Baukas <KarolisBaukas5@gmail.com>
2020-04-10 00:25:54 +0300

    Updates

---
+++ precache-manifest.33dedb26d5bfa77419a7ee9373170c04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4527fcb870013ab08971121bb1030e2",
+    "url": "/React-Warehouse/index.html"
+  },
+  {
+    "revision": "f6b053248ccbd8d830e6",
+    "url": "/React-Warehouse/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a4723d9cc8df57fa0646",
+    "url": "/React-Warehouse/static/css/main.02c6eea5.chunk.css"
+  },
+  {
+    "revision": "f6b053248ccbd8d830e6",
+    "url": "/React-Warehouse/static/js/2.6692f33e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Warehouse/static/js/2.6692f33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4723d9cc8df57fa0646",
+    "url": "/React-Warehouse/static/js/main.b2768d82.chunk.js"
+  },
+  {
+    "revision": "4651a8045ba486dd0f51",
+    "url": "/React-Warehouse/static/js/runtime-main.37a0daf1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/products/precache-manifest.ebf7add2914bbfb6e3632f9d5030eb3f.js"
+  "/React-Warehouse/precache-manifest.33dedb26d5bfa77419a7ee9373170c04.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/products/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Warehouse/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
