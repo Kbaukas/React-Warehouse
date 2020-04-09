b4bc6b0106121112ea28d94905730ecaeccaf0c7
Karolis Baukas <KarolisBaukas5@gmail.com>
2020-04-10 00:19:08 +0300

    Updates

---
+++ precache-manifest.ebf7add2914bbfb6e3632f9d5030eb3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7958385960196ff7ffe8c3e890166d79",
+    "url": "/products/index.html"
+  },
+  {
+    "revision": "f6b053248ccbd8d830e6",
+    "url": "/products/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b8169d1d324e2272cf4d",
+    "url": "/products/static/css/main.02c6eea5.chunk.css"
+  },
+  {
+    "revision": "f6b053248ccbd8d830e6",
+    "url": "/products/static/js/2.6692f33e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/products/static/js/2.6692f33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8169d1d324e2272cf4d",
+    "url": "/products/static/js/main.4233514d.chunk.js"
+  },
+  {
+    "revision": "7e27781175f2850d9da4",
+    "url": "/products/static/js/runtime-main.615d7284.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Warehouse/precache-manifest.bae375c9de63eaea70e3c055d92dfcd0.js"
+  "/products/precache-manifest.ebf7add2914bbfb6e3632f9d5030eb3f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Warehouse/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/products/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
