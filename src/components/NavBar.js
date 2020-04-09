8afc2824600c14d34300794e747fea37f1e35699
Karolis Baukas <KarolisBaukas5@gmail.com>
2020-04-10 01:35:58 +0300

    Some styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,16 +9,16 @@ class NavBar extends React.Component {
         <div className="col-md-12">
           <Router>
             <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-              <Navbar.Brand href="/products">Warehouse</Navbar.Brand>
+              <Navbar.Brand href="#/products">Warehouse</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                   <Nav.Link href="#products/preview">Preview</Nav.Link>
-                  <Nav.Link href="/products">Products</Nav.Link>
+                  <Nav.Link href="#/products">Products</Nav.Link>
 
                   <NavDropdown title="Meniu" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/products">Table</NavDropdown.Item>
-                    <NavDropdown.Item href="/products/preview#">
+                    <NavDropdown.Item href="#/products">Table</NavDropdown.Item>
+                    <NavDropdown.Item href="#/products/preview">
                       Edit
                     </NavDropdown.Item>
 
